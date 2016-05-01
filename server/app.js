fafbf44edd0b44fe169c8a67b3435fc69dbcd438
eunmo <eunmo@cs.stanford.edu>
2016-05-01 20:41:54 +0900

    style change

--- server/app.js
+++ server/app.js
@@ -1,27 +1,27 @@
 (function () {
 	'use strict';
 
-	var express = require('express');
-	var http = require('http');
-	var path = require('path');
-	var ejs = require('ejs');
-	var bodyParser = require('body-parser');
+	var express = require ('express');
+	var http = require ('http');
+	var path = require ('path');
+	var ejs = require ('ejs');
+	var bodyParser = require ('body-parser');
 
-	var routes = require('./routes/index');
+	var routes = require ('./routes/index');
 
-	var app = express();
+	var app = express ();
 
 	// view engine setup
-	app.set('views', path.join(__dirname, 'views'));
-	app.engine('html', ejs.renderFile);
-	app.set('view engine', 'html');
+	app.set ('views', path.join (__dirname, 'views'));
+	app.engine ('html', ejs.renderFile);
+	app.set ('view engine', 'html');
 	
-	app.use(bodyParser.json());
+	app.use (bodyParser.json ());
 
-	app.use('/', routes);
+	app.use ('/', routes);
 		
-	var server = app.listen(3010, function () {
-		console.log('Express server listening on port ' + server.address().port);
+	var server = app.listen (3010, function () {
+		console.log ('Express server listening on port ' + server.address ().port);
 	});
 
 	module.exports = app;
