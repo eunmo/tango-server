3c4306475a4f86433cdc9fd97be6e96d831624a6
eunmo <eunmo@cs.stanford.edu>
2017-04-02 01:44:19 +0900

    automatically add new lines when adding more than one new words

--- client/js/controller.js
+++ client/js/controller.js
@@ -252,6 +252,12 @@ tangoApp.controller ('AddCtrl', function ($rootScope, $scope, $http, $location, 
 		}
 	};
 
+	$scope.newWordAdded = function (index) {
+		if ($scope.words.length === index + 1 && $scope.words.length > 1) {
+			$scope.newLine ();
+		}
+	};
+
 	$scope.submit = function () {
 		var words = [];
 
