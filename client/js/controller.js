ad0aeee2fd7bbe4b24a417831d8c313d4d44eff0
eunmo <eunmo@cs.stanford.edu>
2017-05-31 17:48:18 +0900

    add color to meta page

--- client/js/controller.js
+++ client/js/controller.js
@@ -13,8 +13,10 @@ tangoApp.controller ('MetaCtrl', function ($rootScope, $scope, $http) {
 	$scope.meta = [];
 	$scope.streaks = [];
 	$scope.levels = [];	
+	$scope.langs = [];
 	$scope.newWordCount = 0;
 	$scope.selectedLevel = '';
+	$scope.selectedLang = '';
 
 	function fixDate (date, streak) {
 		var baseDate = new Date (date);
@@ -28,17 +30,35 @@ tangoApp.controller ('MetaCtrl', function ($rootScope, $scope, $http) {
 		return new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate());
 	}
 
+	function getLang (level) {
+		var head = level.charAt(0);
+		var lang;
+
+		switch (head) {
+			case 'N':
+				lang = 'J';
+				break;
+
+			default:
+				lang = head;
+				break;
+		}
+
+		return lang;
+	}
+
 	$http.get ('meta').success (function (data) {
-		var i;
+		var i, j;
 		var now = new Date();
+		var newDate, streaks;
 
 		for (i = 0; i <= 10; i++) {
-			var newDate = fixDate (new Date (now), 0);
+			newDate = fixDate (new Date (now), 0);
 			newDate.setDate (newDate.getDate() + i);
-			var streaks = [];
+			streaks = [];
 
-			for (var j = 0; j < Math.min(10, 11 - i); j++) {
-				streaks.push ({ sum: 0, levels: {}});
+			for (j = 0; j < Math.min(10, 11 - i); j++) {
+				streaks.push ({ sum: 0, levels: {}, langs: { E: 0, F: 0, J: 0 }});
 			}
 
 			$scope.meta[i] = {
@@ -52,12 +72,13 @@ tangoApp.controller ('MetaCtrl', function ($rootScope, $scope, $http) {
 			$scope.streaks[i] = 0;
 		}
 
+		var row, streak, testDay, index, lang;
 		for (i in data) {
-			var row = data[i];
-			var streak = Math.max(row.streak, 1);
-			var testDay = fixDate (row.lastCorrect, streak);
+			row = data[i];
+			streak = Math.max(row.streak, 1);
+			testDay = fixDate (row.lastCorrect, streak);
 			now = fixDate (new Date (), 0);
-			var index = (testDay - now) / 86400000;
+			index = (testDay - now) / 86400000;
 
 			if (index >= 0) {
 				$scope.streaks[10 - streak] += 1;
@@ -69,6 +90,9 @@ tangoApp.controller ('MetaCtrl', function ($rootScope, $scope, $http) {
 				}
 
 				$scope.meta[index].streaks[10 - streak].levels[row.Level] += 1;
+
+				lang = getLang(row.Level);
+				$scope.meta[index].streaks[10 - streak].langs[lang] += 1;
 			}
 		}
 	});
@@ -111,15 +135,96 @@ tangoApp.controller ('MetaCtrl', function ($rootScope, $scope, $http) {
 			$scope.levels.push(levels[i]);
 		}
 		$scope.levels.sort(function(a, b) { return (a.name < b.name) ? -1 : ((a.name > b.name) ? 1 : 0); });
+
+		var langMap = {};
+		var lang;
+		for (i in $scope.levels) {
+			level = $scope.levels[i];
+			lang = level.name.charAt(0);
+
+			if (langMap[lang] === undefined) {
+				langMap[lang] = { lang: lang, levels: [] };
+			}
+
+			langMap[lang].levels.push(level);
+		}
+
+		for (i in langMap) {
+			$scope.langs.push(langMap[i]);
+		}
+		$scope.langs.sort(function(a, b) { return (a.lang < b.lang) ? -1 : ((a.lang > b.lang) ? 1 : 0); });
 	});
 
 	$scope.selectLevel = function (level) {
 		if ($scope.selectedLevel === level) {
 			$scope.selectedLevel = '';
-		}
-		else {
+			$scope.selectedLang = getLang(level);
+		} else if ($scope.selectedLang === getLang(level)) {
+			$scope.selectedLang = '';
+		} else {
 			$scope.selectedLevel = level;
+			$scope.selectedLang = '';
 		}
+	};
+
+	$scope.filter = function (level) {
+		if ($scope.selectedLevel !== '') {
+			if ($scope.selectedLevel !== level.name)
+				return false;
+		} else if ($scope.selectedLang !== '') {
+			if ($scope.selectedLang !== getLang(level.name))
+				return false;
+		}
+
+		return true;
+	};
+
+	$scope.fullLanguage = function (lang) {
+		switch (lang) {
+			case 'J':
+				return 'Japanese';
+
+			case 'F':
+				return 'French';
+
+			case 'E':
+				return 'English';
+		}
+
+		return lang;
+	};
+
+	$scope.getUpperColor = function (level, streak) {
+		var r, g, b;
+		r = g = b = 255;
+
+		var lang = level.name.charAt(0);
+		switch (lang) {
+			case 'N':
+			case 'J':
+				g -= streak;
+				b -= streak;
+				break;
+
+			case 'F':
+				r -= streak;
+				g -= streak;
+				break;
+
+			case 'E':
+			default:
+				r -= streak;
+				b -= streak;
+				break;
+		}
+
+		var css = 'background-color: rgb(' + r + ',' + g + ',' + b + ')';
+
+		if (streak >= 128) {
+			css += '; color: white';
+		}
+
+		return css;
 	};
 });
 
--- client/js/filter.js
+++ client/js/filter.js
@@ -5,3 +5,20 @@ tangoApp.filter ('hideZero', function () {
 		return num;
 	};
 });
+
+tangoApp.filter ('fullLanguage', function () {
+	return function (lang) {
+		switch (lang) {
+			case 'J':
+				return 'Japanese';
+
+			case 'F':
+				return 'French';
+
+			case 'E':
+				return 'English';
+		}
+
+		return lang;
+	};
+});
