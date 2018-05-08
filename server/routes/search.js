a25604ed2e71cd5c9160ecd93ed4ea39ef861089
eunmo <eunmo@cs.stanford.edu>
2018-05-09 01:53:23 +0900

    update search to split query by common separators

--- server/routes/search.js
+++ server/routes/search.js
@@ -5,10 +5,19 @@
 		
 		router.get ('/search/:_word', function (req, res) {
 			var word = req.params._word;
+
+			var array = word.split(/[\[\(·]/).map(e => { return e.replace(']', '').replace(')', '').trim() });
+			var filters = [];
+
+			for (var i = 0; i < array.length; i++) {
+				word = array[i];
+				filters.push({ word: { $regex: word } });
+				filters.push({ yomigana: { $regex: word } });
+				filters.push({ meaning: { $regex: word } });
+			}
+
 			db.words
-			  .find ({ $or: [ { word: { $regex: word } },
-											  { yomigana: { $regex: word } },
-												{	meaning: { $regex: word } } ] })
+			  .find ({ $or: filters })
 			  .sort ({ Level: 1, index: 1 }, function (err, data) {
 				res.json (data);
 			});
