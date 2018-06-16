745e91f19a9920d23f162f9073ca0be2a7db2d4c
eunmo <eunmo@cs.stanford.edu>
2018-06-17 02:12:37 +0900

    use full-width parenthesis instead of half-width for search delimiter

--- server/routes/search.js
+++ server/routes/search.js
@@ -6,7 +6,7 @@
 		router.get ('/search/:_word', function (req, res) {
 			var word = req.params._word;
 
-			var array = word.split(/[\[\(·]/).map(e => { return e.replace(']', '').replace(')', '').trim() });
+			var array = word.split(/[\[（·]/).map(e => { return e.replace(']', '').replace('）', '').trim() });
 			var filters = [];
 
 			for (var i = 0; i < array.length; i++) {
